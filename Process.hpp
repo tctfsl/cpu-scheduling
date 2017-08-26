ab2e4f7f04c5e69f474fa714aa049986658387de
Faisal <23201989+tctfsl@users.noreply.github.com>
2017-08-26 14:22:24 +0800

    Create Process.hpp

---
+++ Process.hpp
@@ -0,0 +1,27 @@
+#ifndef PROCESS_HPP
+#define PROCESS_HPP
+
+class Process
+{
+  private:
+    int PID;
+    int arriveTime;
+	int burstTime;
+	int priority;
+	int turnaroundTime;
+	int waitTime;
+  public:
+    Process();
+	Process(int PID, int arriveTime, int burstTime, int priority);
+	int getPID();
+	int getArriveTime();
+	int getBurstTime();
+	int getPriority();
+	int getTurnaroundTime();
+	int getWaitTime();
+	void decrementBurstTime();
+	void calculateTurnaroundTime(int endTime);
+	void calculateWaitTime(int burstTime);
+};
+
+#endif
