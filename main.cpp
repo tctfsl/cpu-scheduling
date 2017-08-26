05d7fe4bd29ee52adcf20596d23c956854cd68bb
Faisal <23201989+tctfsl@users.noreply.github.com>
2017-08-26 14:31:23 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include "Process.hpp"
+using namespace std;
+
+void setProcess(vector<Process> &pList);
+void RR(vector<Process> pList);
+
+int main()
+{
+  return 0;
+}
+
+void setProcess(vector<Process> &pList);
+{
+  
+}
+
+void RR(vector<Process> pList)
+{
+  
+}
