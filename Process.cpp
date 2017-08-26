dc11d3bf04c65b15581d4881e6f9933186862d99
Faisal <23201989+tctfsl@users.noreply.github.com>
2017-08-26 14:23:17 +0800

    Create Process.cpp

---
+++ Process.cpp
@@ -0,0 +1,66 @@
+#include "Process.hpp"
+
+Process::Process()
+{
+  this->PID = 0;
+  this->arriveTime = 0;
+  this->burstTime = 0;
+  this->priority = 0;
+  this->turnaroundTime = 0;
+  this->waitTime = 0;
+}
+
+Process::Process(int PID, int arriveTime, int burstTime, int priority)
+{
+  this->PID = PID;
+  this->arriveTime = arriveTime;
+  this->burstTime = burstTime;
+  this->priority = priority;
+  this->turnaroundTime = 0;
+  this->waitTime = 0;
+}
+
+int Process::getPID()
+{
+  return PID;
+}
+
+int Process::getArriveTime()
+{
+  return arriveTime;
+}
+
+int Process::getBurstTime()
+{
+  return burstTime;
+}
+
+int Process::getPriority()
+{
+  return priority;
+}
+
+int Process::getTurnaroundTime()
+{
+  return turnaroundTime;
+}
+
+int Process::getWaitTime()
+{
+  return waitTime;
+}
+
+void Process::decrementBurstTime()
+{
+  burstTime--;
+}
+
+void Process::calculateTurnaroundTime(int endTime)
+{
+  this->turnaroundTime = endTime - this->arriveTime;
+}
+
+void Process::calculateWaitTime(int burstTime)
+{
+  this->waitTime = this->turnaroundTime - burstTime;
+}
