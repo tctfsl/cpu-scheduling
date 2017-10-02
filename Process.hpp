faa2e70711d99c354a926699d5e41d654dafaaf7
Faisal <faisal.mfmy@gmail.com>
2017-10-02 11:47:30 +0800

    Add files via upload

--- Process.hpp
+++ Process.hpp
@@ -4,24 +4,24 @@
 class Process
 {
   private:
-    int PID;
-    int arriveTime;
-    int burstTime;
-    int priority;
-    int turnaroundTime;
-    int waitTime;
+  int PID;
+  int arriveTime;
+  int burstTime;
+  int priority;
+  int turnaroundTime;
+  int waitTime;
   public:
-    Process();
-    Process(int PID, int arriveTime, int burstTime, int priority);
-    int getPID();
-    int getArriveTime();
-    int getBurstTime();
-    int getPriority();
-    int getTurnaroundTime();
-    int getWaitTime();
-    void decrementBurstTime();
-    void calculateTurnaroundTime(int endTime);
-    void calculateWaitTime(int burstTime);
+  Process();
+  Process(int PID, int arriveTime, int burstTime, int priority);
+  int getPID();
+  int getArriveTime();
+  int getBurstTime();
+  int getPriority();
+  int getTurnaroundTime();
+  int getWaitTime();
+  void decrementBurstTime();
+  void calculateTurnaroundTime(int endTime);
+  void calculateWaitTime(int burstTime);
 };
 
-#endif
+#endif //PROCESS_HPP
